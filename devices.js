92fd91f35fd3116874c0f60049f3ff79025478c4
giovanni <144000giovanni@gmail.com>
2023-04-16 01:30:31 +1200

    'update'

--- devices.js
+++ devices.js
@@ -3,10 +3,7 @@
 
 var devices = (function() {
 
-   this.deviceList = {"items": {"sdk4": ["Ionic", "Versa", "Versa Lite", "Versa 2" ],
-           "sdk5": ["Versa 3", "Sense"]
-		  }
-   };
+   this.deviceList = {"items": {"round": "round", "rectangle": "rectangle"}};
     this.get = function() {
 		
 		return this.deviceList;
